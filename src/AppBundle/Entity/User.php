ad8c3c29bbae14e82a882b99f2a8c9206db8586f
maumasi <liu.maumasi@gmail.com>
2017-01-26 17:43:16 -0500

    http test

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liumaumasi
+ * Date: 1/26/17
+ * Time: 3:45 PM
+ */
+
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
