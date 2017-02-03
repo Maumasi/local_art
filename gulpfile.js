a725437c5514bb5895bb8adfa70de22ed1386280
maumasi <liu.maumasi@gmail.com>
2017-02-02 20:49:38 -0500

    started making SASS files. I had to make a visual grid for myself to make the front-end a little easier to work with.

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,19 @@ let gulp = require('gulp'),
     sass = require('gulp-sass');
 
 
+// compile SASS files
 gulp.task('sass', () => {
   return gulp.src('./devAssets/sass/styles.sass')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./web/css/'));
 });
 
-gulp.task('default', () => {
-  console.log('testing gulp');
+
+// watch for sass file changes on save
+gulp.task('watch', () => {
+  gulp.watch('./devAssets/sass/**/*.sass', ['sass']);
 });
+
+
+
+gulp.task('default', ['sass', 'watch']);
