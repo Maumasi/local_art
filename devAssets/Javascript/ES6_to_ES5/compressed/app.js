f577247ec903736e561b32059e550d7f3273479b
maumasi <liu.maumasi@gmail.com>
2017-02-10 15:12:41 -0500

    needing to commit files before merging branches

--- devAssets/Javascript/ES6_to_ES5/compressed/app.js
+++ devAssets/Javascript/ES6_to_ES5/compressed/app.js
@@ -1,5 +1,3 @@
-(function(google) {
-
 "use strict";
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -85,6 +83,3 @@ function options() {
     }
   };
 }
-
-// passing in global namespaces to all the files
-})(google);
